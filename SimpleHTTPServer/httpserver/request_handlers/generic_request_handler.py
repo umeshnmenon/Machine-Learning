d097589df3777b2fe222e9db628a2531384600c6
umeshnmenon <umeshnmenon@gmail.com>
2018-05-07 15:17:07 +0530

    A Simple HTTP Server in Python

---
+++ SimpleHTTPServer/httpserver/request_handlers/generic_request_handler.py
@@ -0,0 +1,21 @@
+import logging
+from autologging import logged
+from httpserver.response_codes import SimpleHTTPStatusOK, SimpleHTTPErrorInternalError
+@logged(logging.getLogger("http.server.log"))
+class GenericRequestHandler:
+    def process_request(self, func=None, args=None):
+        body = []
+        try:
+            if func is None:
+                body = self.index()
+            else:
+                body = getattr(self, func)(args)
+            header = 200
+        except Exception as e:
+            self.__log.error("Error while processing request. Error: {}".format(str(e)))
+            body["error"] = str(e)
+            header = 500
+        return header, body
+
+    def index(self):
+        return {}
---
+++ SimpleHTTPServer/httpserver/request_handlers/helloworld_request_handler.py
@@ -0,0 +1,16 @@
+import logging
+from autologging import logged
+from httpserver.response_codes import SimpleHTTPStatusOK, SimpleHTTPErrorInternalError
+import threading
+from generic_request_handler import GenericRequestHandler
+@logged(logging.getLogger("http.server.log"))
+class HealthcheckRequestHandler(GenericRequestHandler):
+    # def __init__(self, config=None):
+    #     self.config = config
+
+    def hello_world(self, args):
+        """
+        Serve the HTTP Request
+        :return:
+        """
+        return 200, "Hello World! {}".format(args['test_name'])
